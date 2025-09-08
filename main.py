190d8b16df70ddf4673df159590f5830383ea52c
matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2025-09-08 18:23:02 +0200

    Add support for outputting markdown

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 from botasaurus.browser import browser, Driver
 import time
+from markdownify import markdownify as md
 
-# Botasaurus task to get the full HTML of a page
+
 @browser
 def get_page_html_task(driver: Driver, url: str):
     tab = driver.get(url, bypass_cloudflare=True)
-    
     for _ in range(10):
         if "you're not a bot" not in driver.title.lower():
             break
@@ -15,25 +15,26 @@ def get_page_html_task(driver: Driver, url: str):
     time.sleep(.5)
     return tab.get_content()
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def scrape_page_route():
     url_to_scrape = request.args.get('url')
+    response_format = request.args.get('format')
     if not url_to_scrape:
-        return "This is a Flask server that can scrape web pages. Use the /?url=<your_url> endpoint to get the HTML of a page."
-        # return jsonify({"error": "Missing 'url' parameter"}), 400
+        return """<h1>This is a simple proxy server to solve most common anti-bot mechanisms</h1><br>
+        Use the /?url=<your_url> endpoint to get the HTML of a page.<br><br>
+        Use ?format=md to display as markdown instead of html."""
 
     try:
-        # Add http:// if not present to avoid errors with botasaurus
         if not url_to_scrape.startswith(('http://', 'https://')):
             url_to_scrape = 'http://' + url_to_scrape
         
-        # Call the botasaurus task
         page_html = get_page_html_task(url_to_scrape)
+        if response_format == 'md':
+            return md(page_html)
         return page_html
     except Exception as e:
-        # Log the exception for debugging
         print(f"Error during scraping: {e}") 
         return jsonify({"error": f"An unexpected error occurred while scraping: {str(e)}"}), 500
-
